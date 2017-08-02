07bcd632adda1ae63da942b5188ce48abcd56380
L. Robson S.D <robson.tar.gz@gmail.com>
2017-08-02 00:29:03 -0300

    Sintaxe básica.

---
+++ aula01/nocoes-basicas.py
@@ -0,0 +1,32 @@
+# comentário simples
+
+"""
+comentário
+de
+múltiplas
+linhas
+"""
+
+# imprimindo conteúdo
+print('teste')
+
+# variaveis
+n = 10
+
+print(n)
+
+# imprimindo o tipo da variável
+print(type(n))
+
+# tipo nulo no python
+m = None
+print(m)
+
+# float
+n2 = 3.234546456565654
+print(n2)
+print(type(n2))
+
+# manipulação de strings
+print('Robson'.upper())
+print('Robson'.lower())
