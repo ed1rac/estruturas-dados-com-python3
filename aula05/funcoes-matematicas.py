e9d5802f1ecd3f50b69382a3aad1ef8e3dbdda29
L. Robson S.D <robson.tar.gz@gmail.com>
2017-08-03 22:34:20 -0300

    Uso das principais funções matemáticas.

---
+++ aula05/funcoes-matematicas.py
@@ -0,0 +1,39 @@
+"""
+Funções matemáticas
+https://docs.python.org/3/library/math.html?highlight=math#module-math
+
+"""
+
+import math
+
+# menor inteiro maior ou igual a 2.345
+print(math.ceil(2.345))
+
+# valor absoluto de um número
+print(math.fabs(4))
+print(math.fabs(-4))
+
+# maior inteiro menor ou igual a um determinado número
+print(math.floor(3.67))
+print(math.floor(9.17))
+
+# resultado da exponenciação de e (número de Euler / número neperiano) elevado a um determinado número
+print(math.exp(3)) # mesmo resultado que math.pow(math.e, 3)
+
+# logaritmo de n na base 10
+print(math.log10(5))
+
+# resultado de x elevado a y
+print(math.pow(2, 34))
+
+# raiz quadrada
+print(math.sqrt(34))
+print(math.sqrt(9))
+
+# funções trigonométricas
+print(math.cos(41))
+print(math.sin(42))
+print(math.tan(43))
+
+# valor de pi
+print(math.pi)
